b1fe20f554b2af126fbe8736e0810c1a7205b4b4
Jcassp <94863314+Jcassp@users.noreply.github.com>
2023-02-21 12:44:30 -0600

    add the code for the login

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lagin</title>
+ 
+</script>
+    <link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+
+    <section class="container">
+        <div class="login-container">
+            <div class="circle circle-oine"></div>
+            <div class="form-container">
+                <!--imagen de github   
+                <img src="https://raw.githubusercontent.com/hicodersofficial/glassmorphism-login-form/master/assets/illustration.png" alt="illustration" class="illustration" />
+                -->
+                <h1>LOGIN</h1>
+                <form>
+                    <input type="text" placeholder="USERNAME" />
+                    <input type="password" placeholder="PASSWORD" />
+                    <button class="opacity">SUBMIT</button>
+                </form>
+                <div class="register-forget opacity">
+                    <a href="register.php">REGISTER</a>
+                    <!--<a href="">FORGOT PASSWOORD</a>-->
+                </div>
+            </div>
+            <div class="circle circle-two"></div>
+        </div>
+        <div class="theme-btn container"> </div>
+    </section>
+
+</body>
+
+</html>
